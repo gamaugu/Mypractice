70d42f4d8d3e8cf36d25bef0cd743a4e858e9405
HTM.N <tomirinsu@gmail.com>
2021-08-09 22:56:22 +0900

    remember_tokenとemail_verified_atを追加する前。

---
+++ database/migrations/2021_08_09_225429_add_remember_token_to_members_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRememberTokenToMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            //
+        });
+    }
+}
